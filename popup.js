b88d79f3fd0edb0e81b2ea35ab068da32a4b427b
Michael Ren <Mike@Michaels-MacBook-Pro-2.local>
2017-11-04 10:39:36 -0400

    update

--- popup.js
+++ popup.js
@@ -124,3 +124,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+document.addEventListener('XIF', () => {
+  var commentBox = document.createElement('div')
+  var inputBar = document.createElement('input')
+  var submitButton = document.createElement('button')
+
+
+  commentBox.appendChild('inputBar');
+  commentBox.appendChild('submitButton');
+
+  //Here is where we need to attach this Div to the window
+  document.getElementById('comments').appendChild('commentBox')
+
+});
