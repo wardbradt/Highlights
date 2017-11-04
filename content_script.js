8fb1126043c7cf650d16c0d62c150048e8621b52
wardbradt <bbradt@andover.edu>
2017-11-04 13:09:27 -0400

    transition div opacity to 0.9

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 // JavaScript source code
 // Content Script
+// Runs on the page
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
     if (request.method == "getSelection") {
         console.log(window.getSelection().anchorNode);
@@ -8,7 +9,7 @@ chrome.extension.onRequest.addListener(function (request, sender, sendResponse) 
             parentNode: window.getSelection().anchorNode.parentNode.nodeName
         });
         // Create div
-
+        document.getElementById("myDivId").style.opacity = "0.9";
     }
     else
         sendResponse({}); // snub them.
