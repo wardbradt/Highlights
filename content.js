d9f92260cf464626a2a81c812933941a80dc5de1
sam-xif <s.xifaras999@gmail.com>
2017-11-04 14:44:17 -0400

    Fixed content.js

--- content.js
+++ content.js
@@ -22,9 +22,7 @@ submitButton.value = 'Submit';
 
 //append all elements
 document.body.appendChild( div );
-div.appendChild( submitForm );
-submitForm.appendChild( comment );
-submitForm.appendChild( submitButton );
+
 //set attributes for div
 div.id = 'myDivId';
 div.style.position = 'fixed';
@@ -32,11 +30,10 @@ div.style.top = '0%';
 div.style.left = '60%';
 div.style.width = '40%';
 div.style.height = '100%';
-div.style.backgroundColor = 'white';
+div.style.backgroundColor = 'lightblue';
 div.style.opacity = 0.9;
+div.style.zIndex = 2147483647;
 
-//set attributes for submitForm
-submitForm.action = '';
 
 //set attributes for btn
 // "btn.removeAttribute( 'style' );
@@ -45,3 +42,48 @@ submitForm.action = '';
 // submitButton.style.position = 'absolute';
 // submitButton.style.top = '50%';
 // submitButton.style.left = '50%';
+
+// append new message box
+
+// create a div that holds everyting that this funciton creates
+var messageHolder = document.createElement('div');
+messageHolder.style.overflowY = 'auto';
+messageHolder.style.height = '90%';
+for (var i = 0; i >= 100; i++) {
+  var tempDiv = document.createElement('div');
+  var tempText = document.createElement('p');
+  tempText.id = 'text'+ (100-i);
+  tempText.innerHTML = "";
+  tempText.style.paddingTop = "5px";
+  tempText.style.paddingLeft = "5px";
+  tempText.style.paddingRight = "5px";
+  tempText.style.paddingBottom = "5px";
+  tempDiv.style.height = '10%';
+  tempDiv.style.width = '95%';
+  tempDiv.style.border = 'solid';
+  tempDiv.style.borderWidth = '1px';
+  tempDiv.style.margin = '0 auto';
+  tempDiv.appendChild(tempText);
+  messageHolder.appendChild(tempDiv);
+}
+div.appendChild(messageHolder)
+var messageContainer = document.createElement( 'div' );
+div.appendChild(messageContainer);
+messageContainer.appendChild( submitForm );
+submitForm.appendChild( comment );
+submitForm.appendChild( submitButton );
+messageContainer.style.bottom = 0;
+messageContainer.style.position = 'fixed';
+messageContainer.style.width = '100%';
+messageHolder.scrollTop = messageHolder.scrollHeight;
+
+
+//set attributes for submitForm
+submitForm.action = '';
+function appendMessage(message, order)
+{
+  document.getElementById("text" + order).innerHTML = message;
+}
+appendMessage('hello', 0);
+appendMessage('12345', 2);
+appendMessage('asdf', 1);
